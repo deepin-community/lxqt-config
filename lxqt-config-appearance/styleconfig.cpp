4d44f09d7efdaa88ab088c96a62f87c82b2c2494
Deepin Developer <deepin-dev@deepin.org>
2022-10-12 13:21:08 +0800

    feat: Init commit
    init lxqt-config
    
    Log:

---
+++ lxqt-config-appearance/styleconfig.cpp
@@ -0,0 +1,277 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXQt - a lightweight, Qt based, desktop toolset
+ * https://lxqt.org/
+ *
+ * Copyright: 2014 LXQt team
+ * Authors:
+ *   Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#include "styleconfig.h"
+#include "ui_styleconfig.h"
+#include <QTreeWidget>
+#include <QDebug>
+#include <QStyleFactory>
+#include <QToolBar>
+#include <QSettings>
+#include <QMetaObject>
+#include <QMetaProperty>
+#include <QMetaEnum>
+#include <QToolBar>
+
+#ifdef Q_WS_X11
+extern void qt_x11_apply_settings_in_all_apps();
+#endif
+
+StyleConfig::StyleConfig(LXQt::Settings* settings, QSettings* qtSettings, LXQt::Settings *configAppearanceSettings, ConfigOtherToolKits *configOtherToolKits, QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::StyleConfig),
+    mQtSettings(qtSettings),
+    mSettings(settings)
+{
+    mConfigAppearanceSettings = configAppearanceSettings;
+    mConfigOtherToolKits = configOtherToolKits;
+    ui->setupUi(this);
+
+    initControls();
+
+    connect(ui->advancedOptionsGroupBox, &QGroupBox::toggled, this, &StyleConfig::showAdvancedOptions);
+
+    connect(ui->qtComboBox, QOverload<int>::of(&QComboBox::activated), this, &StyleConfig::settingsChanged);
+    connect(ui->advancedOptionsGroupBox, &QGroupBox::clicked, this, &StyleConfig::settingsChanged);
+    connect(ui->gtk2ComboBox, QOverload<int>::of(&QComboBox::activated), this, &StyleConfig::settingsChanged);
+    connect(ui->gtk3ComboBox, QOverload<int>::of(&QComboBox::activated), this, &StyleConfig::settingsChanged);
+    connect(ui->toolButtonStyle, QOverload<int>::of(&QComboBox::activated), this, &StyleConfig::settingsChanged);
+    connect(ui->singleClickActivate, &QAbstractButton::clicked, this, &StyleConfig::settingsChanged);
+
+    connect(ui->winColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->baseColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->highlightColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->windowTextColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->viewTextColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->highlightedTextColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->linkColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+    connect(ui->linkVisitedColorLabel, &ColorLabel::colorChanged, this, &StyleConfig::settingsChanged);
+
+    connect(ui->defaultPaletteBtn, &QAbstractButton::clicked, [this] {
+        QColor winColor(239, 239, 239);
+        QPalette defaultPalette(winColor);
+        ui->winColorLabel->setColor(winColor, true);
+        ui->baseColorLabel->setColor(defaultPalette.color(QPalette::Active,QPalette::Base), true);
+        // Qt's default highlight color may be different from that of Fusion
+        ui->highlightColorLabel->setColor(QColor(60, 140, 230), true);
+        ui->windowTextColorLabel->setColor(defaultPalette.color(QPalette::Active,QPalette::WindowText), true);
+        ui->viewTextColorLabel->setColor(defaultPalette.color(QPalette::Active,QPalette::Text), true);
+        ui->highlightedTextColorLabel->setColor(defaultPalette.color(QPalette::Active,QPalette::HighlightedText), true);
+        ui->linkColorLabel->setColor(defaultPalette.color(QPalette::Active,QPalette::Link), true);
+        ui->linkVisitedColorLabel->setColor(defaultPalette.color(QPalette::Active,QPalette::LinkVisited), true);
+    });
+}
+
+
+StyleConfig::~StyleConfig()
+{
+    delete ui;
+}
+
+
+void StyleConfig::initControls()
+{
+
+    // Fill global themes
+    QStringList qtThemes = QStyleFactory::keys();
+    QStringList gtk2Themes = mConfigOtherToolKits->getGTKThemes(QStringLiteral("2.0"));
+    QStringList gtk3Themes = mConfigOtherToolKits->getGTKThemes(QStringLiteral("3.*"));
+
+    if(!mConfigAppearanceSettings->contains(QStringLiteral("ControlGTKThemeEnabled")))
+        mConfigAppearanceSettings->setValue(QStringLiteral("ControlGTKThemeEnabled"), false);
+    bool controlGTKThemeEnabled = mConfigAppearanceSettings->value(QStringLiteral("ControlGTKThemeEnabled")).toBool();
+
+    showAdvancedOptions(controlGTKThemeEnabled);
+    ui->advancedOptionsGroupBox->setChecked(controlGTKThemeEnabled);
+
+    // read other widget related settings from LXQt settings.
+    QByteArray tb_style = mSettings->value(QStringLiteral("tool_button_style")).toByteArray();
+    // convert toolbar style name to value
+    QMetaEnum me = QToolBar::staticMetaObject.property(QToolBar::staticMetaObject.indexOfProperty("toolButtonStyle")).enumerator();
+    int val = me.keyToValue(tb_style.constData());
+    if(val == -1)
+      val = Qt::ToolButtonTextBesideIcon;
+    ui->toolButtonStyle->setCurrentIndex(val);
+
+    // activate item views with single click
+    ui->singleClickActivate->setChecked( mSettings->value(QStringLiteral("single_click_activate"), false).toBool());
+
+    // Fill Qt themes
+    ui->qtComboBox->clear();
+    ui->qtComboBox->addItems(qtThemes);
+
+    // Fill GTK themes
+    ui->gtk2ComboBox->addItems(gtk2Themes);
+    ui->gtk3ComboBox->addItems(gtk3Themes);
+
+    ui->gtk2ComboBox->setCurrentText(mConfigOtherToolKits->getGTKThemeFromRCFile(QStringLiteral("2.0")));
+    ui->gtk3ComboBox->setCurrentText(mConfigOtherToolKits->getGTKThemeFromRCFile(QStringLiteral("3.0")));
+
+    // Qt style
+    mSettings->beginGroup(QLatin1String("Qt"));
+    ui->qtComboBox->setCurrentText(mSettings->value(QStringLiteral("style")).toString());
+    mSettings->endGroup();
+
+    // palette
+    mSettings->beginGroup(QLatin1String("Palette"));
+    QColor color;
+    color.setNamedColor(mSettings->value(QStringLiteral("window_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::Window);
+    ui->winColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("base_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::Base);
+    ui->baseColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("highlight_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::Highlight);
+    ui->highlightColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("window_text_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::WindowText);
+    ui->windowTextColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("text_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::Text);
+    ui->viewTextColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("highlighted_text_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::HighlightedText);
+    ui->highlightedTextColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("link_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::Link);
+    ui->linkColorLabel->setColor(color);
+
+    color.setNamedColor(mSettings->value(QStringLiteral("link_visited_color")).toString());
+    if (!color.isValid())
+        color = QGuiApplication::palette().color(QPalette::Active,QPalette::LinkVisited);
+    ui->linkVisitedColorLabel->setColor(color);
+
+    mSettings->endGroup();
+
+    update();
+}
+
+void StyleConfig::applyStyle()
+{
+    // Qt style
+    QString themeName = ui->qtComboBox->currentText();;
+    mQtSettings->beginGroup(QLatin1String("Qt"));
+    if(mQtSettings->value(QStringLiteral("style")).toString() != themeName)
+        mQtSettings->setValue(QStringLiteral("style"), themeName);
+    mQtSettings->endGroup();
+
+    // palette
+    mSettings->beginGroup(QLatin1String("Palette"));
+    QColor color = ui->winColorLabel->getColor();
+    QColor oldColor;
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("window_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("window_color"), color.name());
+
+    color = ui->baseColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("base_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("base_color"), color.name());
+
+    color = ui->highlightColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("highlight_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("highlight_color"), color.name());
+
+    color = ui->windowTextColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("window_text_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("window_text_color"), color.name());
+
+    color = ui->viewTextColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("text_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("text_color"), color.name());
+
+    color = ui->highlightedTextColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("highlighted_text_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("highlighted_text_color"), color.name());
+
+    color = ui->linkColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("link_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("link_color"), color.name());
+
+    color = ui->linkVisitedColorLabel->getColor();
+    oldColor.setNamedColor(mQtSettings->value(QStringLiteral("link_visited_color")).toString());
+    if (color != oldColor)
+        mQtSettings->setValue(QStringLiteral("link_visited_color"), color.name());
+
+    mQtSettings->endGroup();
+
+    // single click setting
+    if(mSettings->value(QStringLiteral("single_click_activate")).toBool() !=  ui->singleClickActivate->isChecked()) {
+        mSettings->setValue(QStringLiteral("single_click_activate"), ui->singleClickActivate->isChecked());
+    }
+
+   // tool button style
+   int index = ui->toolButtonStyle->currentIndex();
+    // convert style value to string
+    QMetaEnum me = QToolBar::staticMetaObject.property(QToolBar::staticMetaObject.indexOfProperty("toolButtonStyle")).enumerator();
+    if(index == -1)
+        index = Qt::ToolButtonTextBesideIcon;
+    const char* str = me.valueToKey(index);
+    if(str && mSettings->value(QStringLiteral("tool_button_style")) != QString::fromUtf8(str))
+    {
+        mSettings->setValue(QStringLiteral("tool_button_style"), QString::fromUtf8(str));
+        mSettings->sync();
+        mConfigOtherToolKits->setConfig();
+    }
+
+    if (ui->advancedOptionsGroupBox->isChecked())
+    {
+        // GTK3
+        themeName = ui->gtk3ComboBox->currentText();
+        mConfigOtherToolKits->setGTKConfig(QStringLiteral("3.0"), themeName);
+        // GTK2
+        themeName = ui->gtk2ComboBox->currentText();
+        mConfigOtherToolKits->setGTKConfig(QStringLiteral("2.0"), themeName);
+        // Update xsettingsd
+        mConfigOtherToolKits->setXSettingsConfig();
+    }
+}
+
+void StyleConfig::showAdvancedOptions(bool on)
+{
+    ui->uniformThemeLabel->setVisible(on);
+    mConfigAppearanceSettings->setValue(QStringLiteral("ControlGTKThemeEnabled"), on);
+}
---
+++ lxqt-config-appearance/translations/lxqt-config-appearance_ar.ts
@@ -0,0 +1,350 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>ColorLabel</name>
+    <message>
+        <location filename="../colorLabel.cpp" line="37"/>
+        <source>Click to change color.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorLabel.cpp" line="61"/>
+        <source>Select Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConfigOtherToolKits</name>
+    <message>
+        <location filename="../configothertoolkits.cpp" line="148"/>
+        <source>GTK themes</source>
+        <translation>سمات جتك</translation>
+    </message>
+    <message>
+        <location filename="../configothertoolkits.cpp" line="149"/>
+        <source>&lt;p&gt;&apos;%1&apos; has been overwritten.&lt;/p&gt;&lt;p&gt;You can find a copy of your old settings in &apos;%2&apos;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;كُتب فوق ”%1“.&lt;/p&gt;&lt;p&gt;ستجد نسخة عن الإعدادات القديمة في ”%2“&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>FontsConfig</name>
+    <message>
+        <location filename="../fontsconfig.ui" line="23"/>
+        <source>Font</source>
+        <translation>الخط</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="30"/>
+        <source>Default font for user interface</source>
+        <translation>خط واجهة المستخدم المبدئي</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="36"/>
+        <source>Font name:</source>
+        <translation>اسم الخط:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="46"/>
+        <source>Style:</source>
+        <translation>النمط:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="53"/>
+        <source>Point size:</source>
+        <translation>حجم النقطة:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="68"/>
+        <source>Normal</source>
+        <translation>عادي</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="73"/>
+        <source>Bold</source>
+        <translation>ثخين</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="78"/>
+        <source>Italic</source>
+        <translation>مائل</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="83"/>
+        <source>Bold Italic</source>
+        <translation>ثخين مائل</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="94"/>
+        <source>The following settings only affect newly started applications</source>
+        <translation>ستؤثّر التعديلات الآتية على التطبيقات التي تبدأ بعد تطبيقها</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="100"/>
+        <source>Use antialias fonts</source>
+        <translation>استخدم خطوطا محسّنة حوافها</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="107"/>
+        <source>Font hinting style:</source>
+        <translation>نمط تلميحات الخطوط:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="118"/>
+        <location filename="../fontsconfig.ui" line="183"/>
+        <source>None</source>
+        <translation>بلا</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="123"/>
+        <source>Slight</source>
+        <translation>قليل</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="128"/>
+        <source>Medium</source>
+        <translation>وسط</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="133"/>
+        <source>Full</source>
+        <translation>كامل</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="141"/>
+        <source>Font hinting</source>
+        <translation>تلميحات الخطوط</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="148"/>
+        <source>Resolution (DPI):</source>
+        <translation>الميز (نقطة/بوصة):</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="155"/>
+        <source>Autohint</source>
+        <translation>التلميحات آليّة</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="172"/>
+        <source>Subpixel antialiasing:</source>
+        <translation>مضادّ تعرّج البكسلات الفرعية:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="188"/>
+        <source>RGB</source>
+        <translation>RGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="193"/>
+        <source>BGR</source>
+        <translation>BGR</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="198"/>
+        <source>VRGB</source>
+        <translation>VRGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="203"/>
+        <source>VBGR</source>
+        <translation>VBGR</translation>
+    </message>
+</context>
+<context>
+    <name>IconThemeConfig</name>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="14"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>ضبط لكسكيوت للمظهر</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="26"/>
+        <source>Icons Theme</source>
+        <translation>سمة الأيقونات</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="87"/>
+        <source>The KDE extension of XDG icon themes -&gt; FollowsColorScheme</source>
+        <translation>امتداد «كدي» لسمات أيقونات XDG ← ‏FollowsColorScheme (يتبع نمط الألوان)</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="90"/>
+        <source>Colorize icons based on widget style (palette)</source>
+        <translation>لوّن الأيقونات حسب نمط الودجات (لوحة الألوان)</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtThemeConfig</name>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="23"/>
+        <source>LXQt Theme</source>
+        <translation>سمة لكسكيوت</translation>
+    </message>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="57"/>
+        <source>Override user-defined wallpaper</source>
+        <translation>تجاهل الخلفية التي عرّفها المستخدم</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="58"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>ضبط لكسكيوت للمظهر</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="71"/>
+        <source>Widget Style</source>
+        <translation>نمط الودجات</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="79"/>
+        <source>Icons Theme</source>
+        <translation>سمة الأيقونات</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="88"/>
+        <source>LXQt Theme</source>
+        <translation>سمة لكسكيوت</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="96"/>
+        <source>Font</source>
+        <translation>الخط</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="106"/>
+        <source>Cursor</source>
+        <translation>المؤشّر</translation>
+    </message>
+</context>
+<context>
+    <name>StyleConfig</name>
+    <message>
+        <location filename="../styleconfig.ui" line="33"/>
+        <source>Widget Style</source>
+        <translation>نمط الودجات</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="63"/>
+        <source>Qt Palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="77"/>
+        <source>Some Qt styles may ignore these colors.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="102"/>
+        <source>Window:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="112"/>
+        <source>View:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="122"/>
+        <source>Selection:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="132"/>
+        <source>Link:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="152"/>
+        <source>Window Text:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="162"/>
+        <source>View Text:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="172"/>
+        <source>Selected Text:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="182"/>
+        <source>Visited Link:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="227"/>
+        <source>&amp;Default Palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="254"/>
+        <source>Set GTK themes (GTK configuration files will be overwritten!)</source>
+        <translation>اضبط سمات جتك (سيُكتب فوق ملفات ضبط جتك!)</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="274"/>
+        <source>GTK 3 Theme</source>
+        <translation>سمة جتك ٣</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="284"/>
+        <source>GTK 2 Theme</source>
+        <translation>سمة جتك ٢</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="294"/>
+        <source>To attempt uniform theming, either select similar style/theme (if available) across all lists, or select &apos;gtk2&apos; Qt style (if available) to mimic GTK themes.
+
+Make sure &apos;xsettingsd&apos; is installed to help GTK applications apply themes on the fly.</source>
+        <translation>إن أردت توحيد السمات فاختر إما سمة أو نمطًا متشابهًا في كل القوائم (إن توفّر)، أو اختر نمط كيوت ”gtk2“ (إن توفّر) لمحاكاة سمات جتك.
+
+تثبّت أن ”xsettingsd“ مثبّت لتُطبّق تطبيقات جتك السمات فورًا.</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="311"/>
+        <source>Toolbar button style:</source>
+        <translation>نمط زر شريط الأدوات:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="319"/>
+        <source>Only display the icon</source>
+        <translation>اعرض الأيقونة فقط</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="324"/>
+        <source>Only display the text</source>
+        <translation>اعرض النص فقط</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="329"/>
+        <source>The text appears beside the icon</source>
+        <translation>يظهر النص بجانب الأيقونة</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="334"/>
+        <source>The text appears under the icon</source>
+        <translation>يظهر النص أسفل الأيقونة</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="339"/>
+        <source>Default</source>
+        <translation>المبدئي</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="347"/>
+        <source>Activate item on single click</source>
+        <translation>فعّل العنصر بالنقر المفرد</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="51"/>
+        <source>Qt Style</source>
+        <translation>نمط كيوت</translation>
+    </message>
+</context>
+</TS>
